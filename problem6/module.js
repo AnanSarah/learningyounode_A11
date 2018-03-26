cb6ee91e3a3c1e026d522baec7f038cf9c026a67
AnanSarah <anansarahnikita27@gmail.com>
2018-03-27 00:54:54 +0600

    program 6 update

--- problem6/module.js
+++ problem6/module.js
@@ -1,11 +1,13 @@
+var fs = require('fs');
+var path = require('path');
  module.exports = function (dirpath, ext, callback ){
-   fs.readdir(dirpath, function callback (err, list){
-     if(err) return callback(err);
-        else{
-           for(i=0; i<list.length; i++){
-           if(path.extname(list[i]) == ext){
-           }
-         }
-       }
-   })
+   fs.readdir(dirpath, function(err, list){
+     if (err)
+      return callback(err)
+      list = list.filter(function(file){
+          return path.extname(file) === '.' + ext;
+        })
+        // console.log(list);
+        callback(null, list)
+      })
  }
--- problem6/program6.js
+++ problem6/program6.js
@@ -1,4 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 var module = require('./module.js')
-module(process.argv[2],process.argv[3], callback(err,list))
+module(process.argv[2],process.argv[3], function(err,list){
+    if (err) {return console.error('There was an error:', err)}
+    list.forEach(function (file) {
+    console.log(file)
+    })
+})
