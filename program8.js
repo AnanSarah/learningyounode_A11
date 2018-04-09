7a9311e27460e8704461650cd8547abdd1d28ee2
AnanSarah <anansarahnikita27@gmail.com>
2018-04-09 19:32:17 +0600

    p7,p8

---
+++ program8.js
@@ -0,0 +1,13 @@
+var http= require('http');
+var url = process.argv[2];
+const bl= require('bl');
+http.get(url, function(response){
+  response.pipe(bl(function (err, data) {
+    if (err) {
+           return console.error(err);
+       }
+       data = data.toString();
+       console.log(data.length);
+       console.log(data);
+     }));
+})
